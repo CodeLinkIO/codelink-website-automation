6c8e570e83715b654e2392a2e89c39a192dce56f
Thao Tran <42793908+ThaoThaoTran@users.noreply.github.com>
2024-01-05 11:15:12 +0700

    verify services pages are displayed correctly

--- src/Codelink_Website/core/pageObjects/HomePage.java
+++ src/Codelink_Website/core/pageObjects/HomePage.java
@@ -2,6 +2,7 @@ package pageObjects;
 
 import common.BasePage;
 import interfaces.HomePageUI;
+import interfaces.ServicesUI;
 import org.openqa.selenium.WebDriver;
 import pageObjects.ContactUsPage;
 
@@ -15,5 +16,20 @@ public class HomePage extends BasePage {
         waitForElementClickable(driver, HomePageUI.CONTACT_US_BTN);
         clickToElement(driver, HomePageUI.CONTACT_US_BTN);
         return new ContactUsPage(driver);
+    }
+
+    public void selectHomeBar(String homeBar) {
+        waitForElementVisible(driver, ServicesUI.DYNAMIC_BUTTON_HOME_BAR,homeBar);
+        hoverMouseToElement(driver, ServicesUI.DYNAMIC_BUTTON_HOME_BAR,homeBar);
+    }
+
+    public void selectItemInHomeBar(String item) {
+        waitForElementVisible(driver, ServicesUI.DYNAMIC_BUTTON_SELECT_HOME_BAR, item);
+        clickToElement(driver, ServicesUI.DYNAMIC_BUTTON_SELECT_HOME_BAR, item);
+    }
+
+    public boolean isTitleDisplayed(String title) {
+        waitForElementVisible(driver, ServicesUI.DYNAMIC_TITLE, title);
+        return isElementDisplayed(driver, ServicesUI.DYNAMIC_TITLE, title);
     }
 }
