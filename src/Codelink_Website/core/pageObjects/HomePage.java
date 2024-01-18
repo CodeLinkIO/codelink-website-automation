ae142605faa1a97a841cd27f459fc8e2a47a3a23
Thao Tran <42793908+ThaoThaoTran@users.noreply.github.com>
2024-01-18 14:30:42 +0700

    fix comment

--- src/Codelink_Website/core/pageObjects/HomePage.java
+++ src/Codelink_Website/core/pageObjects/HomePage.java
@@ -2,34 +2,18 @@ package pageObjects;
 
 import common.BasePage;
 import interfaces.HomePageUI;
-import interfaces.ServicesUI;
 import org.openqa.selenium.WebDriver;
-import pageObjects.ContactUsPage;
 
 public class HomePage extends BasePage {
     WebDriver driver;
 
-    public HomePage(WebDriver driver){
-        this.driver= driver;
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
     }
+
     public ContactUsPage navigateToContactUsPage() {
         waitForElementClickable(driver, HomePageUI.CONTACT_US_BTN);
         clickToElement(driver, HomePageUI.CONTACT_US_BTN);
         return new ContactUsPage(driver);
-    }
-
-    public void selectHomeBar(String homeBar) {
-        waitForElementVisible(driver, ServicesUI.DYNAMIC_BUTTON_HOME_BAR,homeBar);
-        hoverMouseToElement(driver, ServicesUI.DYNAMIC_BUTTON_HOME_BAR,homeBar);
-    }
-
-    public void selectItemInHomeBar(String item) {
-        waitForElementVisible(driver, ServicesUI.DYNAMIC_BUTTON_SELECT_HOME_BAR, item);
-        clickToElement(driver, ServicesUI.DYNAMIC_BUTTON_SELECT_HOME_BAR, item);
-    }
-
-    public boolean isTitleDisplayed(String title) {
-        waitForElementVisible(driver, ServicesUI.DYNAMIC_TITLE, title);
-        return isElementDisplayed(driver, ServicesUI.DYNAMIC_TITLE, title);
     }
 }
--- src/Codelink_Website/core/testcases/ServicesTest.java
+++ src/Codelink_Website/core/testcases/ServicesTest.java
@@ -2,15 +2,14 @@ package testcases;
 
 import common.BaseTest;
 import common.GlobalConstants;
-import pageObjects.HomePage;
+import org.testng.Assert;
+import pageObjects.CommonFunctionsPage;
 import reportConfig.ExtentTestManager;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import ultilities.log;
 
 import java.lang.reflect.Method;
 
@@ -18,63 +17,62 @@ import static reportConfig.ExtentTestManager.logInfo;
 
 public class ServicesTest extends BaseTest {
     WebDriver driver;
-
-    HomePage homePage;
+    CommonFunctionsPage commonFunctionsPage;
 
     @Parameters({"browser", "environmentName"})
     @BeforeClass(alwaysRun = true)
     public void beforeClass(String browser, String envName) {
         driver = getBrowserDriver(browser, envName);
-        homePage = new HomePage(driver);
+        commonFunctionsPage = new CommonFunctionsPage(driver);
     }
 
     @Test
-    public void Services_Links_And_Pages(Method method){
-        ExtentTestManager.startTest(method.getName(),"Home Page");
+    public void Services_Links_And_Pages(Method method) {
+        ExtentTestManager.startTest(method.getName(), "Services Pages");
 
-        log.info("Select Services home bar");
-        homePage.selectHomeBar("Services");
+        logInfo("Select Services in home bar");
+        commonFunctionsPage.selectHomeBar("Services");
 
-        logInfo("Select Artificial Intelligence & Machine Learning");
-        homePage.selectItemInHomeBar("Artificial Intelligence & Machine Learning");
+        logInfo("Open Artificial Intelligence & Machine Learning page");
+        commonFunctionsPage.selectItemInHomeBar("Artificial Intelligence & Machine Learning");
 
         logInfo("Verify Artificial Intelligence & Machine Learning page is displayed correctly");
-        Assert.assertTrue(homePage.isTitleDisplayed("Artificial Intelligence & Machine Learning"));
-        Assert.assertEquals(homePage.getPageURL(driver), GlobalConstants.ARTIFICIAL_INTELLIGENCE_PAGE);
+        Assert.assertTrue(commonFunctionsPage.isTitleDisplayed("Artificial Intelligence & Machine Learning"));
+        Assert.assertEquals(commonFunctionsPage.getPageURL(driver), GlobalConstants.ARTIFICIAL_INTELLIGENCE_PAGE);
 
-        log.info("Select Services home bar");
-        homePage.selectHomeBar("Services");
+        logInfo("Select Services in home bar");
+        commonFunctionsPage.selectHomeBar("Services");
 
-        logInfo("Select Software Development");
-        homePage.selectItemInHomeBar("Software Development");
+        logInfo("Open Software Development page");
+        commonFunctionsPage.selectItemInHomeBar("Software Development");
 
         logInfo("Verify Software Development page is displayed correctly");
-        Assert.assertTrue(homePage.isTitleDisplayed("Software Development"));
-        Assert.assertEquals(homePage.getPageURL(driver), GlobalConstants.SOFTWARE_DEVELOPMENT_PAGE);
+        Assert.assertTrue(commonFunctionsPage.isTitleDisplayed("Software Development"));
+        Assert.assertEquals(commonFunctionsPage.getPageURL(driver), GlobalConstants.SOFTWARE_DEVELOPMENT_PAGE);
 
-        log.info("Select Services home bar");
-        homePage.selectHomeBar("Services");
+        logInfo("Select Services in home bar");
+        commonFunctionsPage.selectHomeBar("Services");
 
-        logInfo("Select Embedded Teams");
-        homePage.selectItemInHomeBar("Embedded Teams");
+        logInfo("Open Embedded Teams page");
+        commonFunctionsPage.selectItemInHomeBar("Embedded Teams");
 
-        logInfo("Verify Embedded Teams page is displayed correctly");
-        Assert.assertTrue(homePage.isTitleDisplayed("Embedded Teams"));
-        Assert.assertEquals(homePage.getPageURL(driver), GlobalConstants.EMBEDDED_TEAMS_PAGE);
+        logInfo("Verify Software Development page is displayed correctly");
+        Assert.assertTrue(commonFunctionsPage.isTitleDisplayed("Embedded Teams"));
+        Assert.assertEquals(commonFunctionsPage.getPageURL(driver), GlobalConstants.EMBEDDED_TEAMS_PAGE);
 
-        log.info("Select Services home bar");
-        homePage.selectHomeBar("Services");
+        logInfo("Select Services in home bar");
+        commonFunctionsPage.selectHomeBar("Services");
 
-        logInfo("Select Start-up Growth");
-        homePage.selectItemInHomeBar("Start-up Growth");
+        logInfo("Open Start-up Growth page");
+        commonFunctionsPage.selectItemInHomeBar("Start-up Growth");
 
         logInfo("Verify Start-up Growth page is displayed correctly");
-        Assert.assertTrue(homePage.isTitleDisplayed("Start-up Growth"));
-        Assert.assertEquals(homePage.getPageURL(driver), GlobalConstants.START_UP_GROWTH_PAGE);
+        Assert.assertTrue(commonFunctionsPage.isTitleDisplayed("Start-up Growth"));
+        Assert.assertEquals(commonFunctionsPage.getPageURL(driver), GlobalConstants.START_UP_GROWTH_PAGE);
     }
 
     @AfterClass(alwaysRun = true)
-    public void afterClass(){
+    public void afterClass() {
         closeBrowserDriver();
     }
 }
